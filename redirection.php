ea706cd0aea2d726e6f9fed3a30858e142db2ac1
Y0hy0h <y0hy0h@users.noreply.github.com>
2016-09-08 13:34:34 +0200

    Fix: Refactor redirection.php to adapt to previous name changes.
    Names changed from `hide_page` to `redirect_page`.

--- redirection.php
+++ redirection.php
@@ -10,7 +10,7 @@
  */
 
 /**
- * Redirects marked pages.
+ * Redirects marked posts and pages.
  *
  * If the metabox data specifies this post to be hidden, it is
  * redirected to the destination specified in the plugin's settings.
@@ -21,10 +21,10 @@
  */
 function rtr_redirect() {
 	global $post;
-	$hide_page            = get_post_meta( $post->ID, 'rtr_metabox_hide_page', true );
+	$redirect_page        = get_post_meta( $post->ID, 'rtr_metabox_redirect_page', true );
 	$redirect_destination = rtr_get_option( 'rtr_setting_redirect_destination' );
 
-	if ( $hide_page ) {
+	if ( $redirect_page ) {
 		wp_redirect( $redirect_destination );
 	}
 }
