9e8e779162a388f49c331f1dba01980e4a15fb66
Y0hy0h <y0hy0h@users.noreply.github.com>
2016-09-09 16:25:03 +0200

    Hiding: Extend hiding to all menu items with permalink.
    Does not yet work with certain URLs. Only menu items whose URL is correctly interpreted by `url_to_id()` are hidden correctly.

--- hiding.php
+++ hiding.php
@@ -10,12 +10,18 @@
  */
 
 function rtr_hide_from_menu( $items ) {
+	// Contains IDs of all hidden menu items
 	$hidden_posts = array();
 
 	foreach ( $items as $key => $menu_item ) {
-		$referenced_page_id = $menu_item->object_id;
-		$hide_page          = get_post_meta( $referenced_page_id, 'rtr_metabox_hide_page', true );
+		$referenced_page_id = url_to_postid( $menu_item->url );
 
+		// Skip this $menu_item if possible
+		if ( $referenced_page_id == 0 ) { // if page referenced is external, it cannot have metabox data
+			continue;
+		}
+
+		$hide_page           = get_post_meta( $referenced_page_id, 'rtr_metabox_hide_page', true );
 		$menu_item_parent_id = $menu_item->menu_item_parent;
 
 		if ( $hide_page || isset( $hidden_posts[ $menu_item_parent_id ] ) ) {
