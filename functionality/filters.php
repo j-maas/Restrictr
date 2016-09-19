512a1202c3156ed7220de9df67ac5b0716ce95dd
Y0hy0h <Y0hy0h@users.noreply.github.com>
2016-09-19 15:56:07 +0200

    Filter: Implement MMWeg network filter.
    Filters when IP not from the MMWeg intranet.

--- functionality/filters.php
+++ functionality/filters.php
@@ -9,8 +9,13 @@
  * @since 0.4.0
  */
 
+/**
+ * Activates functionality, if determined true.
+ *
+ * @return bool Whether filtering should be active.
+ */
 function rtr_filter() {
-	return ! is_user_logged_in();
+	return ! is_user_in_intranet();
 }
 
 /**
@@ -51,3 +56,46 @@ function rtr_activate_functionality() {
 }
 
 add_action( 'wp_loaded', 'rtr_activate_functionality' );
+
+// IP check
+/**
+ * Checks if client is in subnet.
+ *
+ * @param string $client_ip Human readable client ip
+ * @param string $network_ip Human readable network ip
+ * @param $subnet_mask
+ *
+ * @return bool True, iff client ip is inside the subnet.
+ */
+function is_in_subnet( $client_ip, $network_ip, $subnet_mask ) {
+	$masked_client_ip  = inet_pton( $client_ip ) & inet_pton( $subnet_mask );
+	$masked_network_ip = inet_pton( $network_ip ) & inet_pton( $subnet_mask );
+
+	return $masked_client_ip == $masked_network_ip;
+}
+
+/**
+ * Checks if user comes from MMWeg network.
+ *
+ * @return bool Iff the request ip is inside the MMWeg intranet.
+ */
+function is_user_in_intranet() {
+	$subnetMask          = "255.255.254.0";
+	$networkAddress      = "192.168.122.0";
+	$trustedProxyAddress = [ "127.0.0.1", "::1" ];
+
+	if ( ! empty( $_SERVER['REMOTE_ADDR'] ) ) {
+		$ipAddress = $_SERVER['REMOTE_ADDR'];
+	} else {
+		return false;
+	}
+
+	// Check if forward from trusted proxy
+	if ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] )
+	     and in_array( $ipAddress, $trustedProxyAddress )
+	) {
+		$ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	}
+
+	return is_in_subnet( $ipAddress, $subnetMask, $networkAddress );
+}
